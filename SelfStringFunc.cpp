23a52b15cf41c7469cf87386f5f9100f1091ed75
Knightususa <vanya.yakovlev2008@gmail.com>
2026-09-05 14:35:14 +0300

    Add bool, assert, finish task

--- SelfStringFunc.cpp
+++ SelfStringFunc.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <stdbool.h>
+#include <assert.h>
 
 int   Selfstrlen (const char* str);
 int   Selfputs   (const char* str);
 int   Selfstrcmp (const char* lhs, const char* rhs);
 int   SelfstrNcmp(const char* lhs, const char* rhs, int N);
-int   IsSubStr   (const char* str, const char* substr, int i);
+bool   IsSubStr   (const char* str, const char* substr, int i);
 char* Selfstrstr (const char* str, const char* substr); 
 char* Selfstrdup (const char* str1);
 char* Selfstrcpy (char* dest, const char* src);
@@ -53,6 +55,7 @@ int main()
 // -------------------
 int Selfstrlen(const char* str)
 {
+    assert(str != NULL);
     int i = -1;
     while(str[++i] != '\0'){}
     return i;
@@ -60,6 +63,7 @@ int Selfstrlen(const char* str)
 
 int Selfputs(const char* str)
 {
+    assert(str != NULL);
     int  i = 0;
     char tempc = 0;
     while((tempc = str[i++]) != '\0')
@@ -72,6 +76,8 @@ int Selfputs(const char* str)
 
 char* Selfstrcpy (char* dest , const char* src)
 {
+    assert(dest != NULL);
+    assert(src  != NULL);
     int  i = 0;
     char tempc = 0;
     while((tempc = src[i]) != '\0')
@@ -85,6 +91,8 @@ char* Selfstrcpy (char* dest , const char* src)
 
 char* Selfstrcat (char* dest , const char* src)
 {
+    assert(dest != NULL);
+    assert(src  != NULL);
     int i = 0;
     while(dest[i++] != '\0'){}
     int j = 0;
@@ -97,6 +105,8 @@ char* Selfstrcat (char* dest , const char* src)
 
 int Selfstrcmp(const char* lhs, const char* rhs)
 {
+    assert(lhs != NULL);
+    assert(rhs != NULL);
     int  i = 0;
     char l = 0, r = 0; 
     while(((l = lhs[i]) != '\0') && ((r = rhs[i]) != '\0'))
@@ -113,6 +123,8 @@ int Selfstrcmp(const char* lhs, const char* rhs)
 
 int SelfstrNcmp(const char* lhs, const char* rhs, int N)
 {
+    assert(lhs != NULL);
+    assert(rhs != NULL);
     int  i = 0;
     char l = 0, r = 0; 
     while(((l = lhs[i]) != '\0') && ((r = rhs[i]) != '\0') && i < N - 1)
@@ -129,12 +141,16 @@ int SelfstrNcmp(const char* lhs, const char* rhs, int N)
 
 char* Selfstrstr (const char* str, const char* substr)
 {
+    assert(str    != NULL);
+    assert(substr != NULL);
     char* firstc = NULL;
     int i = 0;
-    int isSubStr = 0;
+    bool isSubStr = 0;
     while(!isSubStr && i < Selfstrlen(str))
     {
-        if(str[i] == substr[0]) isSubStr = IsSubStr(str, substr, i);
+        if(str[i] == substr[0]) 
+            isSubStr = IsSubStr(str, substr, i);
+
         if(isSubStr)
         {
             firstc = (char*) str + i;
@@ -145,23 +161,26 @@ char* Selfstrstr (const char* str, const char* substr)
     return firstc;
 }
 
-int IsSubStr(const char* str, const char* substr, int i)
+bool IsSubStr(const char* str, const char* substr, int i)
 {
+    assert(str    != NULL);
+    assert(substr != NULL);
     int j = 0;
-    while(j < Selfstrlen(substr) && i < Selfstrlen(str))
+    int lenSubstr = Selfstrlen(substr);
+    int lenStr = Selfstrlen(str);
+    while(j < lenSubstr && i < lenStr)
     {
-        if(str[i] != substr[j]) return 0;
+        if(str[i] != substr[j]) 
+            return 0;
         i++;
         j++;
     }
-    i--;
-    j--;
-    if(str[i] != substr[j]) return 0;
     return 1;
 }
 
 char* Selfgetline(char* line, FILE* input, const char end)
 {
+    assert(line != NULL);
     char tempc = getc(input);
     int i = 0;
     while(tempc != end)
@@ -176,7 +195,8 @@ char* Selfgetline(char* line, FILE* input, const char end)
 
 char* Selfstrdup (const char* str1)
 {
-    char* strcopy = (char*) calloc(Selfstrlen(str1), sizeof(char));
+    assert(str1 != NULL);
+    char* strcopy = (char*) calloc(Selfstrlen(str1), sizeof(strcopy[0]));
     int i = 0;
     while(str1[i] != '\0')
     {
@@ -185,8 +205,3 @@ char* Selfstrdup (const char* str1)
     }
     return strcopy;
 }
-
-//TODO: 1) strNcmp
-//      2) strstr
-//      3) getline
-//      4) strdup
